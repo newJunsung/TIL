14a1038d0cc5f46ed41412cfca5fff119701d801
kimjunsung <assd1108@naver.com>
2023-11-02 11:04:07 +0900

    Baekjoon [2417] 정수 제곱근

---
+++ Problem Solve/Baekjoon/[2417] 정수 제곱근/solution.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+left = 0
+right = n
+
+while left <= right:
+    mid = (left + right) // 2
+    if mid ** 2 < n:
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(left)
